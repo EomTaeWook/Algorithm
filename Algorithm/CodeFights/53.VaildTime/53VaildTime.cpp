60973063fa2d32ccf04b2185821a8ef8d4dcdc1e
eomtaewook <trim0917@gmail.com>
2018-02-10 14:40:22 +0900

    초기 커밋.

---
+++ Algorithm/CodeFights/53.VaildTime/53VaildTime.cpp
@@ -0,0 +1,20 @@
+// 53vaildtime.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include<string>
+bool validTime(std::string time) {
+	int index = time.find(":");
+	int hour = std::stoi(time.substr(0, index));
+	int min = std::stoi(time.substr(index + 1, 2));
+	if (hour >= 0 && hour <= 23 && min >= 0 && min <= 59)
+		return true;
+	else
+		return false;
+}
+int main()
+{
+	validTime("24:00");
+    return 0;
+}
+
---
+++ Algorithm/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<int> queue = new PriorityQueue<int>(Order.Descending);
+            queue.Enqueue(5);
+            queue.Enqueue(7);
+            queue.Enqueue(3);
+            queue.Enqueue(7);
+            queue.Enqueue(2);
+            queue.Enqueue(6);
+            queue.Enqueue(9);
+            queue.Enqueue(1);
+            queue.Enqueue(4);
+            queue.Enqueue(8);
+
+            while (queue.Count > 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
