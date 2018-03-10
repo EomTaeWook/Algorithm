cc91cafc775be3b35367f38e49dfcd2f9cebcc18
eomtaewook <trim0917@gmail.com>
2018-03-10 18:48:27 +0900

    QuickSort

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using Algorithm.Sort;
 using DataStructure;
 using System;
 using System.Collections.Generic;
@@ -12,22 +13,28 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            PriorityQueue<int> queue = new PriorityQueue<int>(Order.Descending);
-            queue.Append(5);
-            queue.Append(7);
-            queue.Append(3);
-            queue.Append(7);
-            queue.Append(2);
-            queue.Append(6);
-            queue.Append(9);
-            queue.Append(1);
-            queue.Append(4);
-            queue.Append(8);
+            int[] input = { 20, 18, 50, 40, 9, 19, 5,25};
 
-            while (queue.Count > 0)
-            {
-                Console.WriteLine(queue.Read());
-            }
+            List<int> list = new List<int>();
+            list.AddRange(input);
+            QuickSort.Sort(list);
+
+            //PriorityQueue<int> queue = new PriorityQueue<int>(Order.Descending);
+            //queue.Append(5);
+            //queue.Append(7);
+            //queue.Append(3);
+            //queue.Append(7);
+            //queue.Append(2);
+            //queue.Append(6);
+            //queue.Append(9);
+            //queue.Append(1);
+            //queue.Append(4);
+            //queue.Append(8);
+
+            //while (queue.Count > 0)
+            //{
+            //    Console.WriteLine(queue.Read());
+            //}
         }
     }
 }
