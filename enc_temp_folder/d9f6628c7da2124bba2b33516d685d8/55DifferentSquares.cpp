50cdfdca1d594a1091de67ab1b18732df810e75a
eomtaewook <trim0917@gmail.com>
2018-03-29 00:14:40 +0900

    55.codeFights 커밋

---
+++ enc_temp_folder/d9f6628c7da2124bba2b33516d685d8/55DifferentSquares.cpp
@@ -0,0 +1,26 @@
+// 55DifferentSquares.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+#include<vector>
+#include<set>
+int differentSquares(std::vector<std::vector<int>> matrix) {
+	std::set<std::vector<int> > ms;
+	for (int i = 1; i<matrix.size(); i++)
+		for (int j = 1; j<matrix[i].size(); j++)
+			ms.insert({ matrix[i - 1][j - 1],matrix[i - 1][j],matrix[i][j - 1],matrix[i][j] });
+	return ms.size();
+}
+int main()
+{
+	std::vector<std::vector<int>> matrix =
+	{ {1, 2, 1 },
+	{ 2, 2, 2 },
+	{ 2, 2, 2 },
+	{ 1, 2, 3 },
+	{ 2, 2, 1 } };
+	differentSquares(matrix);
+    return 0;
+}
+
